8ded75ab2969d97dbb7d0fe4e04fabc0048ce1d1
Susan Shrestha <60747346+5usan@users.noreply.github.com>
2026-06-06 21:13:29 -0400

    Time: 19 ms (60.21%) | Memory: 25.8 MB (56.30%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -4,13 +4,9 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
-        is_duplicate = False
-        length = len(nums)
-        if length <= 1:
-            return is_duplicate
-        nums.sort()
-        for i in range(1, length):
-            if nums[i] == nums[i-1]:
-                is_duplicate = True
-                break
-        return is_duplicate
+        seen = set()
+        for num in nums:
+            if num in seen:
+                return True
+            seen.add(num)
+        return False

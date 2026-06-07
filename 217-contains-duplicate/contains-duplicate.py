7f5845e1ba51d84c53d5717cb10690db4768349f
Susan Shrestha <60747346+5usan@users.noreply.github.com>
2026-06-06 20:58:15 -0400

    Time: 78 ms (9.27%) | Memory: 20.1 MB (96.61%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.py
+++ 217-contains-duplicate/contains-duplicate.py
@@ -5,10 +5,11 @@ class Solution(object):
         :rtype: bool
         """
         is_duplicate = False
-        if len(nums) <= 1:
+        length = len(nums)
+        if length <= 1:
             return is_duplicate
         nums.sort()
-        for i, num in enumerate(nums):
+        for i in range(1, length):
             if nums[i] == nums[i-1]:
                 is_duplicate = True
                 break

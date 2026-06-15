97b7ff2a58d9efcaf31e3662f9f11681275b5e84
Susan Shrestha <60747346+5usan@users.noreply.github.com>
2026-06-14 22:21:59 -0400

    Time: 7 ms (82.13%) | Memory: 13.9 MB (97.26%) - LeetSync

---
+++ 347-top-k-frequent-elements/top-k-frequent-elements.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def topKFrequent(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        occur = {}
+        for num in nums:
+            occur[num] = occur.get(num, 0) + 1
+
+        heap = []
+
+        for key in occur:
+            heapq.heappush(heap, (occur[key], key))
+            if len(heap) > k:
+                heapq.heappop(heap)
+
+        return [i[1] for i in heap]
